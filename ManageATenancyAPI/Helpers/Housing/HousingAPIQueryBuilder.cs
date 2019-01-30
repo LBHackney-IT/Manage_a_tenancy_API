e602ace78793d86f59e19682feb97997f5820f42
LBHMGeorgieva <mirela.georgieva@hackney.gov.uk>
2019-01-30 11:33:51 +0000

    added process type to query

--- ManageATenancyAPI/Helpers/Housing/HousingAPIQueryBuilder.cs
+++ ManageATenancyAPI/Helpers/Housing/HousingAPIQueryBuilder.cs
@@ -879,6 +879,7 @@ namespace ManageATenancyAPI.Helpers.Housing
                 <attribute name='hackney_managerpropertypatchid' />
                 <attribute name='hackney_incidentid' />
                 <attribute name='hackney_transferred' />
+                <attribute name='hackney_processtype' />
                 <attribute name='hackney_process_stage' />
                 <attribute name='hackney_issuelocation' />
                 <attribute name='hackney_traid' />

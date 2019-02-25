9f834d47563913d57668943e9d6c258f55eb21d5
Stefan Benjamin <stefan.benjamin@rainmaker.solutions>
2019-02-25 11:58:39 +0000

    refactor

--- ManageATenancyAPI/Startup.cs
+++ ManageATenancyAPI/Startup.cs
@@ -72,8 +72,8 @@ namespace ManageATenancyAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            var manageATennancyDb = Configuration.GetConnectionString("ManageATenancyDatabase");
-            NLog.GlobalDiagnosticsContext.Set("ManageATenancyDatabase", manageATennancyDb);
+            //add db connection string to logger
+            NLog.GlobalDiagnosticsContext.Set("ManageATenancyDatabase", Configuration.GetConnectionString("ManageATenancyDatabase"));
 
             loggerFactory.AddNLog();
             app.AddNLogWeb();

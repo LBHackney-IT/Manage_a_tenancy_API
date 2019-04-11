c7af105157129f9426c48f1b4f67fc1dbac86e9b
Stefan Benjamin <stefan.benjamin@gmail.com>
2019-04-11 10:04:08 +0100

    refactor token file

--- ManageATenancyAPI/Services/HackneyGetCRM365Token.cs
+++ ManageATenancyAPI/Services/HackneyGetCRM365Token.cs
@@ -1,57 +1,51 @@
 ﻿using ManageATenancyAPI.Interfaces;
+using Microsoft.Extensions.Options;
+using MyPropertyAccountAPI.Configuration;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Configuration;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Protocols;
-using MyPropertyAccountAPI.Configuration;
 
 namespace ManageATenancyAPI.Services
 {
     public class HackneyGetCRM365Token : IHackneyGetCRM365Token
     {
-        private HttpClient httpClient;
+        private readonly HttpClient httpClient;
         private readonly URLConfiguration configuration;
-        private ILoggerAdapter<HackneyGetCRM365Token> logger;
         public HackneyGetCRM365Token(IOptions<URLConfiguration> config)
         {
             configuration = config?.Value;
-            httpClient = new HttpClient();
-            httpClient.BaseAddress = new Uri(configuration.ManageATenancyAPIURL);
+            httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(configuration.ManageATenancyAPIURL)
+            };
         }
 
-     
+
         public async Task<string> getCRM365AccessToken()
         {
             var response = new HttpResponseMessage();
-            var token = "";
             try
             {
-                response = httpClient.GetAsync("/GetCRM365AccessToken").Result;
+                response = await httpClient.GetAsync("/GetCRM365AccessToken");
                 if (!response.IsSuccessStatusCode)
-                {
                     throw new GetCRM365TokenServiceException();
-                }
-                var tokenJsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content.ReadAsStringAsync().Result);
-                token = tokenJsonResponse["result"].ToString();
+
+                var tokenJsonResponse = JsonConvert.DeserializeObject<JObject>(await response.Content.ReadAsStringAsync());
+                var token = tokenJsonResponse["result"].ToString();
+                return token;
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 response.StatusCode = HttpStatusCode.BadRequest;
                 throw new GetCRM365TokenServiceException();
             }
-            return token;
         }
 
         public class GetCRM365TokenServiceException : System.Exception { }
-  
-       
+
+
     }
 }

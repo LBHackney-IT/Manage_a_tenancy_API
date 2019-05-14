5b9c13ad01cad69bab40be6f22fedbde626c9d43
Daniel Williams <daniel@madetech.com>
2019-05-14 15:07:52 +0100

    Refactored GetNewTenancies method and added interface for persisting last run time

--- ManageATenancyAPI.Tests/Unit/Services/TenancyServiceTests.cs
+++ ManageATenancyAPI.Tests/Unit/Services/TenancyServiceTests.cs
@@ -10,23 +10,125 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using ManageATenancyAPI.Helpers.Housing;
 using Xunit;
 
 namespace ManageATenancyAPI.Tests.Unit.Services
 {
     public class TenancyServiceTests
     {
+        private readonly Mock<IClock> _mockClock;
         private readonly Mock<IHackneyHousingAPICall> _mockHousingApiCall;
+        private readonly Mock<ILastRetrieved> _mockLastRetrieved;
         private readonly TenancyService _service;
 
         public TenancyServiceTests()
         {
+            _mockClock = new Mock<IClock>();
             _mockHousingApiCall = new Mock<IHackneyHousingAPICall>();
+            _mockLastRetrieved = new Mock<ILastRetrieved>();
             var mockHousingApiCallBuilder = new Mock<IHackneyHousingAPICallBuilder>();
             var mockCrmToken = new Mock<IHackneyGetCRM365Token>();
-            _service = new TenancyService(mockHousingApiCallBuilder.Object, _mockHousingApiCall.Object, mockCrmToken.Object);
+            _service = new TenancyService(mockHousingApiCallBuilder.Object, _mockHousingApiCall.Object, mockCrmToken.Object, _mockLastRetrieved.Object, _mockClock.Object);
         }
 
+        private static HttpResponseMessage CreateResponseMessage(Dictionary<string, object> response)
+        {
+            var responseObject = JObject.FromObject(response);
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(responseObject.ToString(), System.Text.Encoding.UTF8, "application/json") };
+            return responseMessage;
+        }
+        
+        private void SetupHousingApiResponse(Dictionary<string, object> response)
+        {
+            _mockHousingApiCall.Setup(x => x.getHousingAPIResponse(It.IsAny<HttpClient>(), It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(CreateResponseMessage(response));
+        }
+
+        [Fact]
+        public async Task GetNewTenancies_UpdateLastRunDate()
+        {
+            SetupHousingApiResponse(new Dictionary<string, object>());
+            _mockLastRetrieved.Setup(m => m.UpdateLastRun(It.IsAny<DateTime>())).Verifiable();
+
+            await _service.GetNewTenancies();
+            
+            _mockLastRetrieved.Verify(m => m.UpdateLastRun(It.IsAny<DateTime>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetNewTenancies_UpdateLastRunDate_WithCorrectDateTime()
+        {
+            var dateTime = new DateTime(2003, 08, 27, 14, 24, 52, 33);
+            
+            SetupHousingApiResponse(new Dictionary<string, object>());
+            _mockLastRetrieved.Setup(m => m.UpdateLastRun(It.IsAny<DateTime>())).Verifiable();
+            _mockClock.Setup(m => m.Now()).Returns(dateTime);
+
+            await _service.GetNewTenancies();
+            
+            _mockLastRetrieved.Verify(m => m.UpdateLastRun(dateTime), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetNewTenancies_RetrievesPreviousLastRunDate()
+        {
+            var now = new DateTime(2019, 05, 14, 11, 00, 00);
+            var lastRunTime = new DateTime(1995, 03, 15, 22, 55, 33, 21);
+            
+            SetupHousingApiResponse(new Dictionary<string, object>());
+            _mockClock.Setup(m => m.Now()).Returns(now);
+            _mockLastRetrieved.Setup(m => m.GetLastRun()).Returns(lastRunTime);
+
+            await _service.GetNewTenancies();
+            
+            _mockLastRetrieved.Verify(m => m.GetLastRun(), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetNewTenancies_UsesLastRunDate()
+        {
+            var lastRunTime = new DateTime(1995, 03, 15, 22, 55, 33, 21);
+            var queryDate = lastRunTime.ToCrmQueryFormat();
+
+            var calledQuery = "";
+            _mockHousingApiCall.Setup(x =>
+                    x.getHousingAPIResponse(It.IsAny<HttpClient>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Callback((HttpClient client, string query, string parameter) => { calledQuery = query; })
+                .ReturnsAsync(CreateResponseMessage(new Dictionary<string, object>()));
+            
+            _mockLastRetrieved.Setup(m => m.GetLastRun()).Returns(lastRunTime);
+
+            await _service.GetNewTenancies();
+
+            Assert.Contains(queryDate, calledQuery);
+        }
+
+        [Fact]
+        public async Task GetNewTenancies_UpdatesLastRun_AfterQuery()
+        {
+            var calledQuery = "";
+            _mockHousingApiCall.Setup(x =>
+                    x.getHousingAPIResponse(It.IsAny<HttpClient>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Callback((HttpClient client, string query, string parameter) => { calledQuery = query; })
+                .ReturnsAsync(CreateResponseMessage(new Dictionary<string, object>()));
+            
+            var newLastRunTime = new DateTime(2018, 07, 5);
+            var oldLastRunTime = new DateTime(2018, 06, 10);
+
+            var currentLastRuntime = oldLastRunTime;
+            _mockLastRetrieved.Setup(m => m.UpdateLastRun(It.IsAny<DateTime>())).Callback((DateTime date) =>
+            {
+                currentLastRuntime = date; 
+            });
+            _mockLastRetrieved.Setup(m => m.GetLastRun()).Returns(() => currentLastRuntime);
+            _mockClock.Setup(m => m.Now()).Returns(newLastRunTime);
+            
+            await _service.GetNewTenancies();
+
+            Assert.Contains(oldLastRunTime.ToCrmQueryFormat(), calledQuery);
+        }
+        
         [Fact]
         public async Task GetNewTenancies_ThereAreNewTenancies_ReturnsListOfNewTenancies()
         {
@@ -34,10 +136,7 @@ namespace ManageATenancyAPI.Tests.Unit.Services
             {
                 { "value", GetNewTenanciesResponse() }
             };
-            var responseObject = JObject.FromObject(dict);
-            var responseMessage = new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(responseObject.ToString(), System.Text.Encoding.UTF8, "application/json") };
-            _mockHousingApiCall.Setup(x => x.getHousingAPIResponse(It.IsAny<HttpClient>(), It.IsAny<string>(), It.IsAny<string>()))
-                .ReturnsAsync(responseMessage);
+            SetupHousingApiResponse(dict);
 
             var newTenancies = await _service.GetNewTenancies();
 
@@ -52,10 +151,7 @@ namespace ManageATenancyAPI.Tests.Unit.Services
             {
                 { "value", new string[0] }
             };
-            var responseObject = JObject.FromObject(dict);
-            var responseMessage = new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(responseObject.ToString(), System.Text.Encoding.UTF8, "application/json") };
-            _mockHousingApiCall.Setup(x => x.getHousingAPIResponse(It.IsAny<HttpClient>(), It.IsAny<string>(), It.IsAny<string>()))
-                .ReturnsAsync(responseMessage);
+            SetupHousingApiResponse(dict);
 
             var newTenancies = await _service.GetNewTenancies();
 
--- ManageATenancyAPI/Services/TenancyService.cs
+++ ManageATenancyAPI/Services/TenancyService.cs
@@ -12,14 +12,30 @@ using System.Threading.Tasks;
 
 namespace ManageATenancyAPI.Services
 {
+    public interface IClock
+    {
+        DateTime Now();
+    }
+    
+    public interface ILastRetrieved
+    {
+        void UpdateLastRun(DateTime timestamp);
+        DateTime GetLastRun();
+    }
+    
     public class TenancyService : ITenancyService
     {
         private readonly HttpClient _client;
         private readonly IHackneyHousingAPICall _manageATenancyAPI;
+        private readonly ILastRetrieved _lastRetrieved;
+        private readonly IClock _clock;
 
-        public TenancyService(IHackneyHousingAPICallBuilder apiCallBuilder, IHackneyHousingAPICall apiCall, IHackneyGetCRM365Token accessToken)
+        public TenancyService(IHackneyHousingAPICallBuilder apiCallBuilder, IHackneyHousingAPICall apiCall,
+            IHackneyGetCRM365Token accessToken, ILastRetrieved lastRetrieved, IClock clock)
         {
             _manageATenancyAPI = apiCall;
+            _lastRetrieved = lastRetrieved;
+            _clock = clock;
 
             var token = accessToken.getCRM365AccessToken().Result;
             _client = apiCallBuilder.CreateRequest(token).Result;
@@ -27,96 +43,117 @@ namespace ManageATenancyAPI.Services
 
         public async Task<IEnumerable<NewTenancyResponse>> GetNewTenancies()
         {
-            var dateFrom = DateTime.Today.AddDays(-1);
-            var query = HousingAPIQueryBuilder.GetNewTenanciesSinceDate(dateFrom);
-
+            var lastRun = _lastRetrieved.GetLastRun();
+            var query = HousingAPIQueryBuilder.GetNewTenanciesSinceDate(lastRun);
+            _lastRetrieved.UpdateLastRun(_clock.Now());
+            
             var result = await _manageATenancyAPI.getHousingAPIResponse(_client, query, null);
 
-            if (result != null)
+            if (result == null)
             {
-                if (!result.IsSuccessStatusCode)
+                return null;
+            }
+
+            if (!result.IsSuccessStatusCode)
+            {
+                throw new TenancyServiceException();
+            }
+
+            var response = JsonConvert.DeserializeObject<dynamic>(await result.Content.ReadAsStringAsync());
+
+            if (response == null || response["value"] == null)
+            {
+                return null;
+            }
+
+            var value = response["value"].ToObject<List<dynamic>>();
+
+            var newTenancies = new List<NewTenancyResponse>();
+            foreach (var entry in value)
+            {
+                var accountId = (string) entry["accountid"];
+                var housingTenure = (string) entry["housing_tenure"];
+                var fullAddress = (string) entry["contact1_x002e_address1_composite"];
+                var existingNewTenancy = newTenancies.FirstOrDefault(x =>
+                    x.AccountId == accountId &&
+                    x.HousingTenure == housingTenure &&
+                    x.FullAddress == fullAddress
+                );
+
+                if (existingNewTenancy != null)
                 {
-                    throw new TenancyServiceException();
-                }
-
-                var response = JsonConvert.DeserializeObject<dynamic>(await result.Content.ReadAsStringAsync());
-
-                if (response == null || response["value"] == null)
-                    return null;
-
-                var value = response["value"].ToObject<List<dynamic>>();
-
-                var newTenancies = new List<NewTenancyResponse>();
-                foreach (var entry in value)
-                {
-                    var accountId = (string)entry["accountid"];
-                    var housingTenure = (string)entry["housing_tenure"];
-                    var fullAddress = (string)entry["contact1_x002e_address1_composite"];
-                    var existingNewTenancy = newTenancies.FirstOrDefault(x =>
-                        x.AccountId == accountId &&
-                        x.HousingTenure == housingTenure &&
-                        x.FullAddress == fullAddress
-                    );
-                    if (existingNewTenancy != null)
+                    if (existingNewTenancy.Contacts.FirstOrDefault(x =>
+                            x.ContactId == entry["contact1_x002e_contactid"].ToString()) != null)
                     {
-                        if (existingNewTenancy.Contacts.FirstOrDefault(x => x.ContactId == entry["contact1_x002e_contactid"].ToString()) != null)
-                            continue;
+                        continue;
+                    }
 
-                        var newContact = new NewTenancyContact
-                        {
-                            FirstName = entry["contact1_x002e_firstname"],
-                            LastName = entry["contact1_x002e_lastname"],
-                            Responsible = entry["contact1_x002e_hackney_responsible"],
-                            Title = entry["contact1_x002e_hackney_title"],
-                            ContactId = entry["contact1_x002e_contactid"]
-                        };
+                    var newContact = CreateNewTenancyContact(entry);
 
-                        //ensure that a responsible contact is always first in the list
-                        if (newContact.Responsible)
-                            existingNewTenancy.Contacts.Insert(0, newContact);
-                        else
-                            existingNewTenancy.Contacts.Add(newContact);
+                    //ensure that a responsible contact is always first in the list
+                    if (newContact.Responsible)
+                    {
+                        existingNewTenancy.Contacts.Insert(0, newContact);
                     }
                     else
                     {
-                        var newTenancy = new NewTenancyResponse
-                        {
-                            AccountCreatedOn = entry["createdon"],
-                            AccountId = entry["accountid"],
-                            HousingTenure = entry["housing_tenure"],
-                            AddressLine1 = entry["contact1_x002e_address1_line1"],
-                            AddressLine2 = entry["contact1_x002e_address1_line2"],
-                            AddressLine3 = entry["contact1_x002e_address1_line3"],
-                            EstateAddress = entry["hackney_propertyareapatch2_x002e_hackney_estateaddress"],
-                            FullAddress = entry["contact1_x002e_address1_composite"],
-                            NeighbourhoodOffice = entry["hackney_propertyareapatch2_x002e_hackney_neighbourhoodofficedesc"],
-                            PostCode = entry["contact1_x002e_address1_postalcode"],
-                            OfficerId = entry["estateOfficerId"],
-                            OfficerName = entry["OfficerFullName"],
-                            HouseholdId = entry["_hackney_household_accountid_value"],
-                            PatchId = entry["hackney_estateofficerpatch3_x002e_hackney_estateofficerpatchid"],
-                            TagReference = entry["housing_tag_ref"],
-                            AreaId = entry["hackney_propertyareapatch2_x002e_hackney_areaname"],
-                            ManagerId = entry["hackney_propertyareapatch2_x002e_hackney_managerpropertypatchid"],
-                            Contacts = new List<NewTenancyContact>
-                            {
-                                new NewTenancyContact
-                                {
-                                    FirstName = entry["contact1_x002e_firstname"],
-                                    LastName = entry["contact1_x002e_lastname"],
-                                    Responsible = entry["contact1_x002e_hackney_responsible"],
-                                    Title = entry["contact1_x002e_hackney_title"],
-                                    ContactId = entry["contact1_x002e_contactid"]
-                                }
-                            }
-                        };
-                        newTenancies.Add(newTenancy);
+                        existingNewTenancy.Contacts.Add(newContact);
                     }
                 }
-                return newTenancies;
+                else
+                {
+                    newTenancies.Add(CreateNewTenancyResponse(entry));
+                }
             }
 
-            return null;
+            return newTenancies;
+        }
+
+        private static NewTenancyContact CreateNewTenancyContact(dynamic entry)
+        {
+            return  new NewTenancyContact
+            {
+                FirstName = entry["contact1_x002e_firstname"],
+                LastName = entry["contact1_x002e_lastname"],
+                Responsible = entry["contact1_x002e_hackney_responsible"],
+                Title = entry["contact1_x002e_hackney_title"],
+                ContactId = entry["contact1_x002e_contactid"]
+            };
+        }
+
+        private static NewTenancyResponse CreateNewTenancyResponse(dynamic entry)
+        {
+            return new NewTenancyResponse
+            {
+                AccountCreatedOn = entry["createdon"],
+                AccountId = entry["accountid"],
+                HousingTenure = entry["housing_tenure"],
+                AddressLine1 = entry["contact1_x002e_address1_line1"],
+                AddressLine2 = entry["contact1_x002e_address1_line2"],
+                AddressLine3 = entry["contact1_x002e_address1_line3"],
+                EstateAddress = entry["hackney_propertyareapatch2_x002e_hackney_estateaddress"],
+                FullAddress = entry["contact1_x002e_address1_composite"],
+                NeighbourhoodOffice = entry["hackney_propertyareapatch2_x002e_hackney_neighbourhoodofficedesc"],
+                PostCode = entry["contact1_x002e_address1_postalcode"],
+                OfficerId = entry["estateOfficerId"],
+                OfficerName = entry["OfficerFullName"],
+                HouseholdId = entry["_hackney_household_accountid_value"],
+                PatchId = entry["hackney_estateofficerpatch3_x002e_hackney_estateofficerpatchid"],
+                TagReference = entry["housing_tag_ref"],
+                AreaId = entry["hackney_propertyareapatch2_x002e_hackney_areaname"],
+                ManagerId = entry["hackney_propertyareapatch2_x002e_hackney_managerpropertypatchid"],
+                Contacts = new List<NewTenancyContact>
+                {
+                    new NewTenancyContact
+                    {
+                        FirstName = entry["contact1_x002e_firstname"],
+                        LastName = entry["contact1_x002e_lastname"],
+                        Responsible = entry["contact1_x002e_hackney_responsible"],
+                        Title = entry["contact1_x002e_hackney_title"],
+                        ContactId = entry["contact1_x002e_contactid"]
+                    }
+                }
+            };
         }
     }
 }

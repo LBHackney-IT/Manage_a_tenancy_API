70e3e9c360cd14c246a1e7cf3170909333e43cc4
Pinkham Software Ltd <jeff@pinkhamsoftware.com>
2019-06-25 12:01:28 +0100

    pull from environment variable

--- ManageATenancyAPI.Tests/Unit/Services/JWTServiceTests.cs
+++ ManageATenancyAPI.Tests/Unit/Services/JWTServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using ManageATenancyAPI.Services;
+﻿using System;
+using ManageATenancyAPI.Services;
 using Xunit;
 using FluentAssertions;
 using ManageATenancyAPI.Services.JWT;
@@ -13,7 +14,7 @@ namespace ManageATenancyAPI.Tests.Unit.Services
         public JWTServiceTests()
         {
             _token = "eyJhbGciOiJIUzI1NiIsImtpZCI6IkhTMjU2IiwidHlwIjoiSldUIn0.eyJzdWIiOiJtaG9sZGVuIiwianRpIjoiIiwiQ3JlYXRlIG1lZXRpbmciOiJ7XCJlc3RhdGVPZmZpY2VyTG9naW5JZFwiOlwiMWYxYmI3MjctY2UxYi1lODExLTgxMTgtNzAxMDZmYWE2YTMxXCIsXCJvZmZpY2VySWRcIjpcIjFmMWJiNzI3LWNlMWItZTgxMS04MTE4LTcwMTA2ZmFhNmEzMVwiLFwidXNlcm5hbWVcIjpcIm1ob2xkZW5cIixcImZ1bGxOYW1lXCI6XCJNZWdhbiBIb2xkZW5cIixcImFyZWFNYW5hZ2VySWRcIjpcIjU1MTJjNDczLTk5NTMtZTgxMS04MTI2LTcwMTA2ZmFhZjhjMVwiLFwib2ZmaWNlclBhdGNoSWRcIjpcIjhlOTU4YTM3LTg2NTMtZTgxMS04MTI2LTcwMTA2ZmFhZjhjMVwiLFwiYXJlYUlkXCI6XCI2XCJ9IiwibmJmIjowLCJleHAiOjE1OTMwNzY2MjYsImlhdCI6MTU2MTQ1NDIyNiwiaXNzIjoiT3V0c3lzdGVtcyIsImF1ZCI6Ik1hbmFnZUFUZW5hbmN5In0.d7e_bDz1JnZdXjDASng67HWmC7s466lfQEDK-weyXCQ";
-            _secret = "eyJhbGciOiJIUzI1NiIsImtpZCI6Ikw0d2Y4QVJXeWdEUl9rMlRvenI0b2xoSkNuaGczVVYwMWVHVFE3WDJsLVkiLCJ0eXAiOiJKV1QifQ";
+            _secret = Environment.GetEnvironmentVariable("HmacSecret");
             _classUnderTest = new JWTService();
         }
 

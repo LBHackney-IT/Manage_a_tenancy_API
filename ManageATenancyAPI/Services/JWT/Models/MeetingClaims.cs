aee2aa82d68e2300171f21211b362924a141f532
lbhssandilya <33062415+lbhssandilya@users.noreply.github.com>
2019-06-28 11:29:50 +0100

    added to create Jwt for a single meeting
    added to create Jwt for a single meeting

---
+++ ManageATenancyAPI/Services/JWT/Models/MeetingClaims.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManageATenancyAPI.Services.JWT.Models
+{
+    public class MeetingClaims : IMeetingClaims
+    {
+       public Guid TraMeetingId { get ; set ; }
+    }
+}
